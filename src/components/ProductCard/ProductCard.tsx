23297a2765ac2bcc8bc19735c9515b514ae41383
youhei usami <usao9110@icloud.com>
2026-08-21 09:32:14 +0400

    update data

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./ProductCard.scss";
+import { Product } from "@/types/product";
+
+type ProductCardProps = {
+  product: Product;
+};
+
+export default function ProductCard() {
+  return <div></div>;
+}
