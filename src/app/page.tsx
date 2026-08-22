33eef4f0062d1d2641f158151c22788275d6b7a0
youhei usami <usao9110@icloud.com>
2026-08-22 09:40:54 +0400

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-
 import React from "react";
+import ProductCard from "@/components/ProductCard/ProductCard";
+import { products } from "@/data/products";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Ceramic Store</h1>
+
+      <div className={styles.productGrid}>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </main>
   );
 }
