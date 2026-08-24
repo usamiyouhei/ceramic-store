ca0e95b85d4a0a2df0b5295e41e9d3777fe15a69
youhei usami <usao9110@icloud.com>
2026-08-24 09:22:44 +0400

    update image

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+type ProductPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
 
-export default function page() {
+export default function ProductPage() {
   return <div></div>;
 }
