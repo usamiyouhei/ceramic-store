411e121142a3b358d0a82595b490bc97ee7839fd
youhei usami <usao9110@icloud.com>
2026-08-30 08:39:47 +0400

    update cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,16 @@
+"use client";
 import React from "react";
+import styles from "./page.module.scss";
+import { useCartStore } from "@/store/cartStore";
 
-export default function page() {
-  return <div></div>;
+export default function CartPage() {
+  const items = useCartStore((state) => state.items);
+  const removeItems = useCartStore((state) => state.removeItem);
+  const increaseQuantity = useCartStore((state) => state.increaseQuantity);
+  const decreaseQuantity = useCartStore((state) => state.decreaseQuantity);
+  const totalPrice = items.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0,
+  );
+  return <main className={styles.main}></main>;
 }
